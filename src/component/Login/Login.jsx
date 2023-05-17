aabb04cf72ec492397692e62fb11e44f68043dc8
Abu-Sahad <abusahad603@gmail.com>
2023-05-17 12:18:30 +0600

    login page added

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,7 +1,52 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { Link } from "react-router-dom";
+import app from "../../firebase/firebase.config";
+
 const Login = () => {
+    const auth = getAuth(app);
+
+    const handleLogInSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, name)
+        signInWithEmailAndPassword(auth, email, password)
+        .
+
+    }
     return (
         <div>
-
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col ">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl py-6 lg:ms-6 font-bold">Please Login now!</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleLogInSubmit} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password"
+                                    name="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <Link to="/register" className="label-text-alt link link-hover text-cyan-400">Please Register now ?</Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,44 +1,73 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from "../../firebase/firebase.config";
+import { Link } from "react-router-dom";
+
+
 const Register = () => {
+    const auth = getAuth(app);
+
+    const handleRegisterSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name,email, password)
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+
+
+
+    }
+
+
     return (
-            <div>
-                <div className="hero min-h-screen bg-base-200">
-                    <div className="hero-content flex-col lg:flex-row-reverse">
-                        <div className="text-center lg:text-left">
-                            <h1 className="text-5xl font-bold py-6">Register now!</h1>
-                           
-                        </div>
-                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                            <div className="card-body">
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Name</span>
-                                    </label>
-                                    <input type="text" placeholder="name" className="input input-bordered" />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Email</span>
-                                    </label>
-                                    <input type="text" placeholder="email" className="input input-bordered" />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Password</span>
-                                    </label>
-                                    <input type="text" placeholder="password" className="input input-bordered" />
-                                    {/* <label className="label">
-                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                    </label> */}
-                                </div>
-                                <div className="form-control mt-6">
-                                    <button className="btn btn-primary">Login</button>
-                                </div>
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold py-6">Register now!</h1>
+
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleRegisterSubmit} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text"
+                                    name="name" placeholder="name" className="input input-bordered" required />
                             </div>
-                        </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password"
+                                    name="password" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <Link to="/login" className="label-text-alt link link-hover text-cyan-400">Al ready have and account?</Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-       
+        </div>
+
     );
 };
 
